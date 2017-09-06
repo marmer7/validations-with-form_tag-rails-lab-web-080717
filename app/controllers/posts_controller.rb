18585b992cf5a6bc2e3dd50c465ccdcc416498c7
Marlon Merjos <marlonmerjos@gmail.com>
2017-09-05 21:52:37 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+
+
   def show
     @post = Post.find(params[:id])
   end
@@ -9,10 +11,12 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-
     @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.valid?
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
